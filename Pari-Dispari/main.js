75ff9ffd65438b01d809fb87eb7012fff1cd1f87
graziana-ciccarelli <grazianaciccarelli99@gmail.com>
2025-01-15 15:40:11 +0100

    aggiunta funzione e variabili (pari-dispari)

---
+++ Pari-Dispari/main.js
@@ -0,0 +1,12 @@
+//Funzione per generare un numero random tra 1 a 5
+
+function numeroRandom(){
+    return Math.floor(Math.random() * 5) + 1;
+}
+
+//Chiedere all'utente di scegliere pari o dispari
+let userChoice = prompt("Scegli 'pari' o 'dispari':");
+
+//Scelta utente del numero da 1 a 5
+let userNumber = parseInt(prompt("Inserisci un numero da 1 a 5"));
+
